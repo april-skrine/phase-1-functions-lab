0b2d94da83475c33e0ce2b3e9a680680bed9f09b
April Skrine <april.skrine@gmail.com>
2022-01-11 10:17:34 -0600

    commit tree 97b39b1cf7bbc53dc76b434f0745c18553554cca

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(num) {
+    return Math.abs(42-num);
+}
+
+function distanceFromHqInFeet(endLocation) {
+    return distanceFromHqInBlocks(endLocation) * 264;
+}
+
+function distanceTravelledInFeet (startLoc, endLoc) {
+    return (Math.abs(endLoc - startLoc) * 264);
+}
+
+function calculatesFarePrice (startLoc, endLoc) {
+    let distanceTraveledInFeet = distanceTravelledInFeet(startLoc, endLoc);
+    if (distanceTraveledInFeet <= 400) {
+        return 0;
+    } else if (distanceTraveledInFeet > 400 && distanceTraveledInFeet < 2000) {
+        return (distanceTraveledInFeet - 400) * .02;
+    } else if (distanceTraveledInFeet >= 2000 && distanceTraveledInFeet < 2500) {
+        return 25;
+    } else if (distanceTraveledInFeet >= 2500) {
+        return 'cannot travel that far';
+    }
+}
